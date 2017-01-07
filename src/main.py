whbrewer/spc

commit d810b7d4d497b29ff3d6afd9dbc972ca43e68df5

in /case update code to only do repeated AJAX calls for jobs in Run state

src/main.py
@@ -194,7 +194,8 @@ def case():
     app = request.query.app
     cid = request.query.cid
     jid = request.query.jid or -1
-    
+    state = jobs(id=jid).state
+
     if re.search("/", cid):
         (u, c) = cid.split("/")
         sid = request.query.sid # id of item in shared
@@ -204,7 +205,7 @@ def case():
 
         params = { 'cid': cid, 'app': app, 'jid': jid, 'contents': output,
                    'sid': sid, 'user': u, 'fn': fn, 'apps': myapps.keys(),
-                   'sched': config.sched }
+                   'sched': config.sched, 'state': state }
         return template('case_public', params)
 
     else:
@@ -213,10 +214,11 @@ def case():
         result = db(jobs.cid==cid).select().first()
         desc = result['description']
         shared = result['shared']
+
         params = { 'cid': cid, 'app': app, 'jid': jid,
                    'user': user, 'fn': fn, 'apps': myapps.keys(),
                    'description': desc, 'shared': shared,
-                   'sched': config.sched  }
+                   'sched': config.sched, 'state': state }
         return template('case', params)
 
 @get('/output')

views/case.tpl
@@ -62,15 +62,18 @@ function show(update_interval) {
           setTimeout(showOutput, update_interval);
           // slow down updates over time to relieve burden on server
           if (update_interval < 10000) {
-              update_interval+=500;
+              update_interval += 500;
           }
        })
     }
     //$("#selector").hide()
     showOutput();
-}
-% if not sched == "ws":
-  show(1000); // comment out if using websocket
+} 
+
+% if state == "C" or state == "X":
+  show(2**31-1) // don't page reload unless in Run or Queue state
+% elif not sched == "ws":
+  show(1000) // comment out if using websocket
 % end
 </script>
 

commit 62bedbc148cb69cc45b4a9bb3a60056d677748ec

fix bug that occurred when opening shared cases (introduced in d810b7d4d)

src/main.py
@@ -96,12 +96,13 @@ def confirm_form():
 
     if config.worker == 'remote':
 
+        request.forms['np'] = 1
         request.forms['desc'] = desc
         request.forms['appmod'] = pickle.dumps(myapps[app])
 
         try:
             print config.remote_worker_url + '/execute'
-            resp = requests.post(config.remote_worker_url +'/execute', data=dict(request.forms))
+            resp = requests.post(config.remote_worker_url +'/execute', data=dict(request.forms), verify=False)
 
         except:
             return template('error', err="failed to submit job to SPC worker. " + \
@@ -194,10 +195,10 @@ def case():
     app = request.query.app
     cid = request.query.cid
     jid = request.query.jid or -1
-    state = jobs(cid=cid).state
 
     if re.search("/", cid):
         (u, c) = cid.split("/")
+        state = jobs(cid=c).state
         sid = request.query.sid # id of item in shared
         run_dir = os.path.join(myapps[app].user_dir, u, myapps[app].appname, c)
         fn = os.path.join(run_dir, myapps[app].outfn)
@@ -209,6 +210,7 @@ def case():
         return template('case_public', params)
 
     else:
+        state = jobs(cid=cid).state
         run_dir = os.path.join(myapps[app].user_dir, user, myapps[app].appname, cid)
         fn = os.path.join(run_dir, myapps[app].outfn)
         result = db(jobs.cid==cid).select().first()

src/worker_ssl.py
@@ -1,4 +1,6 @@
 # This is an SSL worker for submitting jobs remotely
+# with some code borrowed from: 
+# https://github.com/nickbabcock/bottle-ssl/blob/master/main.py
 
 # to use this must install the following package using apt-get or yum:
 #   python-dev