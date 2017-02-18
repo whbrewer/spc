a62fe47f229a9b70d3af5717614d7c5104914373
Wes Brewer <wes@fluidphysics.com>
2017-02-18 16:00:40 +0900

    space out args according to PEP-8

--- src/migrate.py
+++ src/migrate.py
@@ -10,70 +10,70 @@ class dal(object):
         self.groups =  self.db.define_table('groups', Field('id', 'integer'),
                                          Field('name', 'string'))
 
-        self.users = self.db.define_table('users', Field('id','integer'),
+        self.users = self.db.define_table('users', Field('id', 'integer'),
                                          Field('user', 'string'),
-                                         Field('passwd','string'),
-                                         Field('email','string'),
-                                         Field('new_shared_jobs','integer'),
-                                         Field('priority','integer'),
+                                         Field('passwd', 'string'),
+                                         Field('email', 'string'),
+                                         Field('new_shared_jobs', 'integer'),
+                                         Field('priority', 'integer'),
                                          Field('gid', self.db.groups, ondelete="SET NULL"))
 
-        self.apps = self.db.define_table('apps', Field('id','integer'),
-                                       Field('name','string'),
-                                       Field('description','string'),
-                                       Field('category','string'),
-                                       Field('language','string'),
-                                       Field('input_format','string'),
-                                       Field('command','string'),
-                                       Field('preprocess','string'),
-                                       Field('postprocess','string'),
+        self.apps = self.db.define_table('apps', Field('id', 'integer'),
+                                       Field('name', 'string'),
+                                       Field('description', 'string'),
+                                       Field('category', 'string'),
+                                       Field('language', 'string'),
+                                       Field('input_format', 'string'),
+                                       Field('command', 'string'),
+                                       Field('preprocess', 'string'),
+                                       Field('postprocess', 'string'),
                                        Field('gid', self.db.groups, ondelete="SET NULL"))
 
-        self.app_user = self.db.define_table('app_user', Field('id', 'integer'), 
+        self.app_user = self.db.define_table('app_user', Field('id', 'integer'),
                                        Field('appid', 'integer'),
                                        Field('uid', 'integer'))
 
-        self.jobs = self.db.define_table('jobs', Field('id','integer'),
-                                       Field('uid',self.db.users),
-                                       Field('app','string'),
-                                       Field('cid','string'),
+        self.jobs = self.db.define_table('jobs', Field('id', 'integer'),
+                                       Field('uid', self.db.users),
+                                       Field('app', 'string'),
+                                       Field('cid', 'string'),
                                        Field('gid', self.db.groups),
                                        Field('command', 'string'),
-                                       Field('state','string'),
-                                       Field('time_submit','string'),
-                                       Field('walltime','string'),
-                                       Field('description','string'),
-                                       Field('np','integer'),
-                                       Field('priority','integer'),
-                                       Field('starred','string'),
-                                       Field('shared','string'))
+                                       Field('state', 'string'),
+                                       Field('time_submit', 'string'),
+                                       Field('walltime', 'string'),
+                                       Field('description', 'string'),
+                                       Field('np', 'integer'),
+                                       Field('priority', 'integer'),
+                                       Field('starred', 'string'),
+                                       Field('shared', 'string'))
 
-        self.plots = self.db.define_table('plots', Field('id','integer'),
-                                         Field('appid',self.db.apps),
-                                         Field('ptype','string'),
-                                         Field('title','string'),
-                                         Field('options','string'))
+        self.plots = self.db.define_table('plots', Field('id', 'integer'),
+                                         Field('appid', self.db.apps),
+                                         Field('ptype', 'string'),
+                                         Field('title', 'string'),
+                                         Field('options', 'string'))
 
-        self.datasource = self.db.define_table('datasource', Field('id','integer'),
-                                                   Field('pltid',self.db.plots),
-                                                   Field('filename','string'),
-                                                   Field('cols','string'),
-                                                   Field('line_range','string'),
-                                                   Field('data_def','string'))
+        self.datasource = self.db.define_table('datasource', Field('id', 'integer'),
+                                                   Field('pltid', self.db.plots),
+                                                   Field('filename', 'string'),
+                                                   Field('cols', 'string'),
+                                                   Field('line_range', 'string'),
+                                                   Field('data_def', 'string'))
 
-        self.aws_creds = self.db.define_table('aws_creds', Field('id','integer'),
-                                                 Field('key','string'),
-                                                 Field('secret','string'),
-                                                 Field('account_id','string'),
-                                                 Field('uid',self.db.users))
+        self.aws_creds = self.db.define_table('aws_creds', Field('id', 'integer'),
+                                                 Field('key', 'string'),
+                                                 Field('secret', 'string'),
+                                                 Field('account_id', 'string'),
+                                                 Field('uid', self.db.users))
 
         self.aws_instances = self.db.define_table('aws_instances',
-                                                        Field('id','integer'),
-                                                        Field('region','string'),
-                                                        Field('instance','string'),
-                                                        Field('itype','string'),
-                                                        Field('rate','double'),
-                                                        Field('uid',self.db.users))
+                                                        Field('id', 'integer'),
+                                                        Field('region', 'string'),
+                                                        Field('instance', 'string'),
+                                                        Field('itype', 'string'),
+                                                        Field('rate', 'double'),
+                                                        Field('uid', self.db.users))
 
         self.disciplines = self.db.define_table('disciplines', Field('name'))
 
