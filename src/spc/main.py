3a569a45eaee18f169fd92ab1fc063d32382a20e
Wes Brewer <wes@fluidphysics.com>
2017-09-02 11:18:40 +0530

    remove exception traceback

--- src/spc/main.py
+++ src/spc/main.py
@@ -66,9 +66,7 @@ def active_app():
     try:
         return s[APP_SESSION_KEY]
     except:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        print traceback.print_exception(exc_type, exc_value, exc_traceback)
-        return ''
+        return None
 
 def set_active(app):
     # set a session variable to keep track of the current app
