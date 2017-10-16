f9250e2d2cd13ea4f5447c28d22f34629eba6f60
Wes Brewer <wes@fluidphysics.com>
2017-10-16 15:47:08 -0500

    change directory readthedocs.io will look for index.rst

--- docs/conf.py
+++ docs/conf.py
@@ -345,5 +345,5 @@ html_context = {
     "github_user": "whbrewer", # Username
     "github_repo": "spc", # Repo name
     "github_version": "master", # Version
-    "conf_py_path": "/docs/source/", # Path in the checkout to the docs root
+    "conf_py_path": "/docs", # Path in the checkout to the docs root
 }
